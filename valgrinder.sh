c20d8a76194eb0c25be0e7e635ee1c0da116fb9e
Matthias SRUN <msrun@e1r10p9.42.fr>
2018-05-16 12:05:14 +0200

    script enhanced

--- valgrinder.sh
+++ valgrinder.sh
@@ -1,9 +1,10 @@
 #/bin/sh
-make -C src/libft/
-gcc src/update_binary.c src/libft/libftprintf.a -o binary/update_binary
-gcc src/malloc.c -shared -fPIC -o binary/malloc
+BASEDIR=$(dirname "$0")
+make -C $BASEDIR/src/libft/
+gcc $BASEDIR/src/update_binary.c $BASEDIR/src/libft/libftprintf.a -o $BASEDIR/binary/update_binary
+gcc $BASEDIR/src/malloc.c -shared -fPIC -o $BASEDIR/binary/malloc
 
-find $1 -type f -name '*.o' -exec ./binary/update_binary '{}' \;
-gcc `find $1 -type f -name '*.o'` src/malloc.c -o segfaulted
+find $1 -type f -name '*.o' -exec ./$BASEDIR/binary/update_binary '{}' \;
+gcc `find $1 -type f -name '*.o'` $BASEDIR/src/malloc.c -o $BASEDIR/segfaulted
 export NB_MALLOC=$3
-valgrind ./segfaulted $2
+valgrind ./$BASEDIR/segfaulted $2
